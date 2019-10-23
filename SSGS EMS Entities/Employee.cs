368696dd9760181fcb9ef0267b63ce721b86f557
smr2804 <56909672+smr2804@users.noreply.github.com>
2019-10-23 12:29:33 +0530

    Add files via upload

---
+++ SSGS EMS Entities/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SSGS_EMS_Entities
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            EmpId = 0;
+            EmpFirstName = "";
+            EmpLastName = "";
+            EmpDesignation = "";
+            EmpDepartment = "";
+            EmpReportsTo = "";
+        }
+        
+        public Employee(int empId, string empFirstName, string empLastName, string empDesignation, string empDepartment, string empReportsTo)
+        {
+            EmpId = empId;
+            EmpFirstName = empFirstName;
+            EmpLastName = empLastName;
+            EmpDesignation = empDesignation;
+            EmpDepartment = empDepartment;
+            EmpReportsTo = empReportsTo;
+        }
+
+        public Employee(string FirstName, string LastName)
+        {
+            
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=SSGS EMS Data;Integrated Security=SSPI");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM EmpMaster WHERE EmpFirstName='" + FirstName + "' AND EmpLastName='" + LastName + "'", con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            dr.Read();
+            this.EmpId = int.Parse(dr["EmpId"].ToString());
+            this.EmpFirstName = dr["EmpFirstName"].ToString();
+            this.EmpLastName = dr["EmpLastName"].ToString();
+            this.EmpDesignation = dr["EmpDesignation"].ToString();
+            this.EmpDepartment = dr["EmpDepartment"].ToString();
+            this.EmpReportsTo = dr["EmpReportsTo"].ToString();
+            dr.Close();
+        }
+
+        
+
+        public int EmpId;
+        public string EmpFirstName;
+        public string EmpLastName;
+        public string EmpDesignation;
+        public string EmpDepartment;
+        public string EmpReportsTo;
+    }
+}
---
+++ SSGS EMS Entities/EmployeeOptionalInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+
+namespace SSGS_EMS_Entities
+{
+    public class EmployeeOptionalInfo
+    {
+        public EmployeeOptionalInfo()
+        {
+            EmpId = 0;
+            EmpEduInfo = "";
+            EmpFamilyInfo = "";
+            EmpHobbies = "";
+        }
+        public EmployeeOptionalInfo(int empId, string empEduInfo, string empFamilyInfo, string empHobbies)
+        {
+            EmpId = empId;
+            EmpEduInfo = empEduInfo;
+            EmpFamilyInfo = empFamilyInfo;
+            EmpHobbies = empHobbies;
+        }
+        public int EmpId;
+        public string EmpEduInfo;
+        public string EmpFamilyInfo;
+        public string EmpHobbies;
+    }
+}
